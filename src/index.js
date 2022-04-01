a8ed1b8864f21c72813545e64964507e639d4108
Nikolay Lapshin <nlapshin1989@gmail.com>
2022-04-01 17:50:55 +0300

    Added cache

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 const express = require('express')
+const Redis = require('ioredis')
+
 const app = express()
+const redis = new Redis({
+  host: process.env.REDIS_HOST || '127.0.0.1',
+  port: process.env.REDIS_PORT || 6379
+});
 
 const PORT = process.env.PORT || 4000
 
+redis.set('cache', 'cache')
+
 app.get('/ping', (req, res) => {
   res.send('pong')
+})
+
+app.get('/cache', (req, res) => {
+  res.send(redis.get('cache'))
 })
 
 app.listen(PORT, () => {
