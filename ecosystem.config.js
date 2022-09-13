884f672c25fcb0eb3ff7afd1cdb0a10658d645fb
Nikolay Lapshin <nlapshin1989@gmail.com>
2022-09-13 19:26:31 +0300

    Update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,19 +3,11 @@ module.exports = {
     name: 'node-proc',
     script: 'src/index.js',
     exec_mode: 'cluster',
-    watch: '.'
-  }],
-
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
+    instances: 4,
+    watch: '.',
+    increment_var: 'PORT',
+    env: {
+      PORT: 4000
     }
-  }
+  }],
 };
